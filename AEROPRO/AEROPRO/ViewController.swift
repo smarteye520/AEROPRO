1fb8cf8b30bace8c2d42d6f3f729a1c3325a2400
Smart Eye <pureness520@outlook.com>
2020-07-21 05:13:35 +0100

    Initial Project

---
+++ AEROPRO/AEROPRO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AEROPRO
+//
+//  Created by Smart Eye on 7/19/20.
+//  Copyright © 2020 smarteye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
